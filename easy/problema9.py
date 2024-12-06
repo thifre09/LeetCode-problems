6b08ed5527314c095812a7818d5e5af79912a096
thifre09 <128859182+thifre09@users.noreply.github.com>
2024-12-06 18:44:37 -0300

    problema9 melhorado

--- easy/problema9.py
+++ easy/problema9.py
@@ -8,4 +8,27 @@ def isPalindrome(x: int) -> bool:
         return False
     
 
-print(isPalindrome(-5555555555555555555555553444444444334444444443555555555555555555555555))
+def isPalindromeBetter(x: int) -> bool:
+    if x < 0:
+        return False
+    else:
+        var1 = x
+        var2 = 0
+        lenx = 0
+        while x != 0:
+            x = x // 10
+            lenx += 1
+        
+        x = var1
+
+        for i in range(lenx):
+            ultimo_num = x % 10
+            x = x // 10
+            var2 += ultimo_num * (10 ** (lenx - i-1))
+    
+        if var1 == var2:
+            return True
+        else:
+            return False
+
+print(isPalindromeBetter(232))
